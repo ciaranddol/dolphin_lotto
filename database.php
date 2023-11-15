8fa02e841f95197bcf3cb64ffb02652e656f7a97
ciaranddol <150662507+ciaranddol@users.noreply.github.com>
2023-11-15 15:32:34 +0000

    Update database.php
    connection to supabase database

--- database.php
+++ database.php
@@ -1,5 +1,26 @@
-user=postgres 
-password=buckbeak1901!
-host=db.fuhzxgckcobvsvhkqppo.supabase.co 
-port=5432 
-dbname=postgres
+<?php
+
+require 'vendor/autoload.php';
+
+use Supabase\SupabaseClient;
+
+// Replace 'your-url' and 'your-key' with your Supabase project URL and key
+$supabaseUrl = 'postgresql://postgres:buckbeak1901!@db.fuhzxgckcobvsvhkqppo.supabase.co:5432/postgres';
+$supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1aHp4Z2NrY29idnN2aGtxcHBvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTk1MTAzMSwiZXhwIjoyMDE1NTI3MDMxfQ.Ylb53-giqsKDbD0iswLSryZM3KzMiKShYaBNowdYnzk';
+
+$supabase = new SupabaseClient($supabaseUrl, $supabaseKey);
+
+// Example query
+$result = $supabase->from('your-table')->select('*')->get();
+
+// Handle the result
+if ($result->error) {
+    // Handle the error
+    echo 'Error: ' . $result->error->message;
+} else {
+    // Process the data
+    $data = $result->data;
+    // Do something with the data
+}
+
+?>
