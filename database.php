7f19fcc625dad5a348684950ba5c3a4ac88c38af
ciaranddol <150662507+ciaranddol@users.noreply.github.com>
2023-11-15 18:46:27 +0000

    Update database.php

--- database.php
+++ database.php
@@ -1,13 +1,1 @@
-<?php
 
-require 'vendor/autoload.php';
-
-use Supabase\SupabaseClient;
-
-// Replace 'your-url' and 'your-key' with your Supabase project URL and key
-$supabaseUrl = 'postgresql://postgres:buckbeak1901!@db.fuhzxgckcobvsvhkqppo.supabase.co:5432/postgres';
-$supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1aHp4Z2NrY29idnN2aGtxcHBvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTk1MTAzMSwiZXhwIjoyMDE1NTI3MDMxfQ.Ylb53-giqsKDbD0iswLSryZM3KzMiKShYaBNowdYnzk';
-
-$supabase = new SupabaseClient($supabaseUrl, $supabaseKey);
-
-?>
