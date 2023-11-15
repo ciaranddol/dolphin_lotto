7579721aa64d0ba104faa0bc2952afbacd0740c9
ciaranddol <150662507+ciaranddol@users.noreply.github.com>
2023-11-15 18:31:46 +0000

    Update process-signup.php

--- process-signup.php
+++ process-signup.php
@@ -4,7 +4,7 @@ if (empty($_POST["name"])) {
     die("Name is required");
 }
 
-if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
     die("Valid email is required");
 }
 
@@ -12,11 +12,11 @@ if (strlen($_POST["password"]) < 8) {
     die("Password must be at least 8 characters");
 }
 
-if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
+if (!preg_match("/[a-z]/i", $_POST["password"])) {
     die("Password must contain at least one letter");
 }
 
-if ( ! preg_match("/[0-9]/", $_POST["password"])) {
+if (!preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain at least one number");
 }
 
@@ -31,12 +31,12 @@ $sql = "INSERT INTO users (name, email, password)
         
 $stmt = $mysqli->stmt_init();
 
-if ( ! $stmt->prepare($sql)) {
+if (!$stmt->prepare($sql)) {
     die("SQL error: " . $mysqli->error);
 }
 
-$stmt->bind_param("sss", $_POST["name"], $_POST["email"], $_POST["password"])
-                  
+$stmt->bind_param("sss", $_POST["name"], $_POST["email"], $_POST["password"]);
+
 if ($stmt->execute()) {
     header("Location: signup-success.html");
     exit;
@@ -47,6 +47,5 @@ if ($stmt->execute()) {
         die($stmt->error . " " . $stmt->errno);
     }
 }
-
 
 ?>
